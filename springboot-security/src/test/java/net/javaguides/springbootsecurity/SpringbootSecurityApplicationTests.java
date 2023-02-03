2631ea10fd1b9aba4d3f2925cb971e6d35502667
123BHUMIKA <111487078+123BHUMIKA@users.noreply.github.com>
2023-02-03 17:02:31 +0530

    Add files via upload

---
+++ springboot-security/src/test/java/net/javaguides/springbootsecurity/SpringbootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.springbootsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
